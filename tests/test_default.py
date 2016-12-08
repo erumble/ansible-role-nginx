73f014e56c076174310374fe04393cafb1b2bd76
Eric Rumble <eric.rumble@sage.com>
2016-12-08 13:33:13 -0500

    fix lint issues

--- tests/test_default.py
+++ tests/test_default.py
@@ -1,22 +1,26 @@
 from testinfra.utils.ansible_runner import AnsibleRunner
 
-testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('group1')
+testinfra_hosts = AnsibleRunner(
+    '.molecule/ansible_inventory').get_hosts('group1')
 
 
 def test_hosts_file(File):
-  hosts = File('/etc/hosts')
-  assert hosts.user == 'root'
-  assert hosts.group == 'root'
+    hosts = File('/etc/hosts')
+    assert hosts.user == 'root'
+    assert hosts.group == 'root'
+
 
 def test_nginx_package(Package):
-  nginx = Package('nginx')
-  assert nginx.is_installed
+    nginx = Package('nginx')
+    assert nginx.is_installed
+
 
 def test_nginx_service(Service):
-  nginx = Service('nginx')
-  assert nginx.is_running
-  assert nginx.is_enabled
+    nginx = Service('nginx')
+    assert nginx.is_running
+    assert nginx.is_enabled
+
 
 def test_vhosts_file(File):
-  vhosts = File('/etc/nginx/sites-available/vhosts.conf')
-  assert not vhosts.exists
+    vhosts = File('/etc/nginx/sites-available/vhosts.conf')
+    assert not vhosts.exists
